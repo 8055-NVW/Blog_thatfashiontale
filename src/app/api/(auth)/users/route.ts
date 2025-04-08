0ed1399925c8b09e86832ae89047704a9a40fa77
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-04-08 18:16:10 +0100

    added POST and Patch methods to users.ts and make defined base url inside tsconfig

--- src/app/api/(auth)/users/route.ts
+++ src/app/api/(auth)/users/route.ts
@@ -1,5 +1,83 @@
 import { NextResponse } from "next/server"
+import connect from "@lib/db";
+import User from "@lib/modals/user";
+import { Types } from "mongoose";
 
-export const GET = () => {
-    return new NextResponse('Hello there!')
+// This will be used in the PATCH request to confirm the correct userId before proceeding(mongoose)
+const ObjectId = require("mongoose").Types.ObjectId;
+
+export const GET = async () => {
+    try {
+        await connect();
+        const users = await User.find();
+        return new NextResponse(JSON.stringify(users), { status: 200 })
+    }
+
+    catch (error: any) {
+        return new NextResponse("Failed: " + error.message, { status: 500 })
+    }
+}
+
+export const POST = async (request: Request) => {
+    try {
+
+        const body = await request.json();
+
+        await connect();
+
+        if (!body.email || !body.username || !body.password) {
+            return new NextResponse("Missing required fields", { status: 400 });
+        }
+        const newUser = new User(body)
+        await newUser.save()
+
+        return new NextResponse(JSON.stringify({ message: "New user created", user: newUser })
+            , { status: 201 }
+        )
+    } catch (error: any) {
+        return new NextResponse("Failed to create new user : " + error.message, { status: 500 })
+    }
+}
+
+export const PATCH = async (request: Request) => {
+    try {
+        const body = await request.json()
+        const { userId, newUsername } = body;
+
+        await connect();
+
+        if (!userId || !newUsername) {
+            return new NextResponse(
+                JSON.stringify({ message: "Id or new Username not found" }),
+                { status: 400 })
+        }
+
+        if (!Types.ObjectId.isValid(userId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid user ID" }),
+                { status: 400 })
+        }
+
+        const updatedUser = await User.findOneAndUpdate(
+            { _id: new ObjectId(userId) },
+            { username: newUsername },
+            { new: true }
+        )
+
+        if (!updatedUser) {
+            return new NextResponse(
+                JSON.stringify({ message: "User not found" }),
+                { status: 400 }
+            )
+        }
+
+        return new NextResponse(
+            JSON.stringify({ massage: "Success. User Updated", user: updatedUser }),
+            { status: 200 }
+        )
+    } catch (error: any) {
+        return new NextResponse("Error in updating user -" + error.message,
+            { status: 500 }
+        )
+    }
 }
