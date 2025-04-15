0e6477da214d17c23c192d6af32472d851cd94be
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-04-15 16:26:12 +0100

    dded logic to posts/route.ts to filter by category

--- src/app/api/(dashboard)/posts/route.ts
+++ src/app/api/(dashboard)/posts/route.ts
@@ -6,17 +6,29 @@ import { Types } from "mongoose";
 
 
 //VIEW posts
-export const GET = async () => {
+export const GET = async (request: Request) => {
     try {
+        const { searchParams } = new URL(request.url);
+        const categoryId = searchParams.get("categoryId");
+
+        if (!categoryId || !Types.ObjectId.isValid(categoryId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid or missing categoryId"}),
+                {status : 400}
+            )
+        }
         await connect();
-        const posts = await Post.find();
+        
 
         //!logic to filter by category
-        // const filter: any ={
-        //     category: new Types.ObjectId(categoryId)
-        // }
-        return new NextResponse(JSON.stringify(posts), { status: 200 })
-        
+        const filter: any ={
+            category: new Types.ObjectId(categoryId)
+        }
+
+        const posts = await Post.find(filter);
+
+        return new NextResponse(JSON.stringify({posts}), { status: 200 })
+
     } catch (error: any) {
         return new NextResponse("Failed to get posts -" + error.message,
             { status: 500 }
@@ -25,11 +37,11 @@ export const GET = async () => {
 }
 
 //CREATE
-export const POST = async(request: Request)=> {
+export const POST = async (request: Request) => {
     try {
         const body = await request.json()
-        const{ title, slug, content} = body;
-        await  connect();
+        const { title, slug, content } = body;
+        await connect();
 
         if (!title || !slug || !content) {
             return new NextResponse(
@@ -38,19 +50,19 @@ export const POST = async(request: Request)=> {
             );
         }
 
-        const newPost = new Post({title, slug, content})
+        const newPost = new Post({ title, slug, content })
         await newPost.save();
 
         return new NextResponse(
             JSON.stringify({ message: "Post successfully created", post: newPost }),
             { status: 201 }
         )
-        
+
     } catch (error: any) {
         return new NextResponse(
             JSON.stringify(`Failed to create post - ${error.message}`),
             { status: 500 }
-        )        
+        )
     }
 }
 
