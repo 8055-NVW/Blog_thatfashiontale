bee81b2ede5c3bc338c7d42a9619d5c8b6988f4d
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-05-06 17:23:31 +0100

    added GET request for comments/route.ts

---
+++ src/app/api/(dashboard)/comments/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server"
+import connect from "@lib/db";
+import Post from "@lib/modals/Post";
+import Comment from "@lib/modals/Comment";
+import { Types } from "mongoose";
+
+// View all Comments
+export const GET = async (request: Request) => {
+    try {
+        const { searchParams } = new URL(request.url);
+        const postId = searchParams.get("postId");
+        const filter: any = {};
+
+        if (!postId || !Types.ObjectId.isValid(postId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid or missing postId" }),
+                { status: 400 }
+            )
+        }
+
+        await connect()
+
+        if (postId) {
+            const post = await Post.findById(postId);
+
+            if (!post) {
+                return new NextResponse(
+                    JSON.stringify({ message: "Post not found" }),
+                    { status: 404 }
+                );
+            }
+
+            filter.post = new Types.ObjectId(postId)
+        }
+
+        const comments = await Comment.find(filter)
+        .populate('user', 'display_name avatar_url')
+        .sort({ createdAt: -1 });
+
+        return new NextResponse(JSON.stringify({comments}), {status: 200})
+    } catch (error: any) {
+        return new NextResponse("Error in fetching comments" + error.message, {
+            status: 500,
+        })
+    }
+}
+
+//Create Comment
