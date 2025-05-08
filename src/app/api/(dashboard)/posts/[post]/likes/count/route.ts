3af5281c42254de51c12c38cf7305149f89a6ac7
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-05-08 16:46:03 +0100

    addedand tested  logic to get like count for posts

---
+++ src/app/api/(dashboard)/posts/[post]/likes/count/route.ts
@@ -0,0 +1,33 @@
+import connect from "@lib/db";
+import Like from "@lib/modals/Like";
+import Post from "@lib/modals/Post";
+import { Types } from "mongoose";
+import { NextResponse } from "next/server";
+
+// GET - Get like count for post
+export const GET = async (request: Request, context: { params: any }) => {
+    try {
+        const postId = await context.params.post
+        if (!postId || !Types.ObjectId.isValid(postId)) {
+            return new NextResponse(
+                JSON.stringify({ message: "Invalid postId" }),
+                { status: 400 }
+            );
+        }
+
+        await connect();
+
+        const count = await Like.countDocuments({ post: postId })
+
+        return new NextResponse(
+            JSON.stringify({ count }),
+            { status: 200 }
+        )
+
+    } catch (error: any) {
+        return new NextResponse(
+            JSON.stringify({ message: "Error in fetching like count", error: error.message }),
+            { status: 500 }
+        )
+    }
+}
