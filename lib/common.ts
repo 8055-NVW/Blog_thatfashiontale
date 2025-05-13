5b296419a45de8e9b8d07048117d35278425b8f9
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-05-13 14:45:23 +0100

    reverted back to previouse state as code was not working. will attempte alternat method

--- lib/common.ts
+++ lib/common.ts
@@ -1,8 +1,6 @@
 import User from "./modals/User";
 
-export async function isSuperuser(userId: any) {
-
-    const user = await User.findById(userId)
-    console.log(user)
-    return user
-}
+// export async function isSuperuser(userId: any) {
+//     const user = await User.findById(userId)
+//     return user
+// }
