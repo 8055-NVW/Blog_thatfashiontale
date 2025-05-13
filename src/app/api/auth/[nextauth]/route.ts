f0d12d0a6e7a47f95d61c2f0ed5e0f495676fb6f
Vivian Lopes <Viv@Evys-MacBook-Pro.local>
2025-05-13 12:22:20 +0100

    cleared some newly created files

--- src/app/api/auth/[nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextAuthOptions } from "next-auth";
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import User from "@lib/modals/User";
-import connect from "@lib/db";
-
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
